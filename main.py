4a651014b7060fbb3a190d27b59a9ca53b017222
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-08-24 01:45:30 +0300

    feat(json-schema): Getting Serialized Objects
    We want to obtain the serialized schema object

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import io
 import json
+from json.decoder import JSONDecodeError
 from textwrap import TextWrapper
 import click
 from src import analyzer
+import logging
+
+LOG = logging.Logger(__name__)
 
 
 @click.command()
 @click.argument("har_data", type=click.File("r"))
 def cli(har_data: io.TextIOWrapper):
-    analyzer.analyze(json.loads(har_data.read()))
+    try:
+        analyzer.analyze(json.loads(har_data.read()))
+    except JSONDecodeError as e:
+        LOG.warning(e, exc_info=e)
 
 
 if __name__ == "__main__":
--- src/analyzer.py
+++ src/analyzer.py
@@ -1,10 +1,30 @@
 import jsonschema
 import json
+import logging
 from typing import Any
+from pathlib import Path
+import python_jsonschema_objects as pjs
+
+from jsonschema.exceptions import ValidationError
+
+LOG = logging.getLogger(__name__)
+schema_path = Path(__file__).parent.parent / "schema.json"
+
+schema = {}
+with open(schema_path) as f:
+    schema: dict[str, Any] = json.loads(f.read())
+
+
+def validate_schema_data(data: dict[str, Any]):
+    jsonschema.validate(data, schema)
 
 
 def analyze(data: dict[str, Any]):
-    schema = {}
-    with open("schema.json") as f:
-        schema: dict[str, Any] = json.loads(f.read())
-    jsonschema.validate(data, schema)
+    try:
+        validate_schema_data(data)
+    except ValidationError as e:
+        LOG.warning(f"ValidationError: {e}")
+        raise e
+    builder = pjs.ObjectBuilder(schema)
+    HarAnalyzer = builder.build_classes(any_of="use-first").HarAnalyzer
+    har_data = HarAnalyzer(log=data.get("log"))
